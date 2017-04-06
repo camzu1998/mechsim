2236f8a764db302e13b2441774ed47e8cb19a91c
camzu1998 <kamillanger4@gmail.com>
2017-04-06 20:59:57 +0200

    First commit and skeleton website

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Strona główna</title>
+    <meta name="description" content="Gra o życiu mechanika">
+    <meta name="author" content="Kamil Langer">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Revalia" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h2><a href="index.php" style="text-decoration: none; font-family: 'Revalia', cursive;">Mechsim</a></h2>
+        </div>
+        <div class="row" style="margin-top: 15%;">
+            <div class="six columns">
+                <form action="zaloguj.php" method="post">
+                    <p>Login:</p>
+                    <input type="text" name="login">
+                    <p>Hasło:</p>
+                    <input type="password" name="haslo"><br>
+                    <input type="submit" name="Zaloguj" value="Zaloguj!">
+                </form>
+                <a href="rejestracja.php">Zarejestruj się!</a>
+            </div>
+            <div class="six columns">
+                <h3>Changelog:</h3>
+                <br>
+                <br>
+                <br>
+            </div>
+        </div>
+        <div class="row"><!-- Miejsce na reklamy --></div>
+    </div>
+    <footer>Wykonał: Kamil Langer &copy; kamillanger4@gmail.com</footer>
+</body>
+</html>
---
+++ rejestracja.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Strona główna</title>
+    <meta name="description" content="Gra o życiu mechanika">
+    <meta name="author" content="Kamil Langer">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Revalia" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h2><a href="index.php" style="text-decoration: none; font-family: 'Revalia', cursive;">Mechsim</a></h2>
+        </div>
+        <div class="row" style="margin-top: 15%;">
+            <form action="rejestruj.php" method="post" style="text-align:center;">
+                <p>Login:</p>
+                <input type="text" name="login">
+                <p>Hasło:</p>
+                <input type="password" name="haslo">
+                <p>Email:</p>
+                <input type="email" name="email"> <br>
+                <input type="submit" name="rejestruj" value="Rejestruj!">
+            </form>
+        </div>
+        <div class="row"><!-- Miejsce na reklamy --></div>
+    </div>
+    <footer>Wykonał: Kamil Langer &copy; kamillanger4@gmail.com</footer>
+</body>
+</html>
